c862b50ce61e2ea7eaec5e5b1f6f11c439df3869
BautistaGaber <bautistagaber@gmail.com>
2023-03-05 23:13:19 -0300

    Add ContinentController

---
+++ Core/Controllers/ContinentController.cs
@@ -0,0 +1,25 @@
+﻿using ChallengeAlternativo.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace ChallengeAlternativo.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContinentController : ControllerBase
+    {
+
+        private readonly IContinentServices _services;
+        public ContinentController(IContinentServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContinents()
+        {
+            return Ok(await _services.GetContinents());
+        }
+    }
+}
