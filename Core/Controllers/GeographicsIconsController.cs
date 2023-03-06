3b852b19554ce393c24a8414fc6758e9d790094a
BautistaGaber <bautistagaber@gmail.com>
2023-03-06 13:32:42 -0300

    Add GeographicsIcons Controller

---
+++ Core/Controllers/GeographicsIconsController.cs
@@ -0,0 +1,57 @@
+﻿using ChallengeAlternativo.Core.Services.Interfaces;
+using ChallengeAlternativo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeAlternativo.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeographicsIconsController : ControllerBase
+    {
+        private readonly IGeographicIconsServices _services;
+        public GeographicsIconsController(IGeographicIconsServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetGeographicIcons()
+        {
+            return Ok(await _services.GetGeographicIcons());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGeographicIcons(GeographicIcons geographicIcons)
+        {
+            var result = await _services.CreateGeographicIcons(geographicIcons);
+            if(result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateGeographicIcons(GeographicIcons geographicIcons)
+        {
+            var result = _services.UpdateGeographicIcons(geographicIcons);
+            if(result == null)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteGeographicIcons(int id)
+        {
+            var result =  _services.DeleteGeographicIcons(id);
+            if (result == null)
+            {
+              return BadRequest();
+            }
+              return Ok();
+        }
+    }
+}
