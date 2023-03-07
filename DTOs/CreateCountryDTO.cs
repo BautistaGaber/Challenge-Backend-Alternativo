759b2c33b57b7a82b3089e759e1eb68527e3c08f
BautistaGaber <bautistagaber@gmail.com>
2023-03-07 19:34:14 -0300

    Add DTOs

--- DTOs/CreateCountryDTO.cs
+++ DTOs/CreateCountryDTO.cs
@@ -4,21 +4,9 @@ namespace ChallengeAlternativo.DTOs
 {
     public class CreateCountryDTO
     {
-        [Required]
-        [Display(Name = "Image")]
         public string Image { get; set; }
-
-        [Required]
-        [Display(Name = "Denomination")]
         public string Denomination { get; set; }
-
-        [Required]
-        [Display(Name = "Habitants")]
         public int Habitants { get; set; }
-
-        [Required]
-        [Display(Name = "TotalArea")]
         public decimal TotalArea { get; set; }
-
     }
 }
